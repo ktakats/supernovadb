c480a2dbf2dd94dd82c8aea29d2c77dd3ddbd70e
ktakats <ktakats@gmail.com>
2017-02-17 17:42:56 -0300

    Prettification

--- SNe/tables.py
+++ SNe/tables.py
@@ -7,7 +7,7 @@ TEMPLATE='<a href="/sn/{{sn.id}}/obslog/edit/{{record.pk}}/"><i class="fa fa-pen
 class ObsLogTable(tables.Table):
     obs_date=tables.DateColumn(short=True, verbose_name='Date')
     obs_type=tables.Column(verbose_name="Type")
-    options=tables.TemplateColumn(TEMPLATE)
+    options=tables.TemplateColumn(TEMPLATE, orderable=False)
 
     class Meta:
         model=Obs
