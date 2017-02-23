bd2d96f7f9337d87d8894c6a7be62b4e121e2dbb
ktakats <ktakats@gmail.com>
2017-02-23 17:11:15 -0300

    HTML template refactoring

---
+++ SNe/static/javascripts/deleteModal.js
@@ -0,0 +1,11 @@
+
+
+function delModal(sn_id, loc){
+  $('#confirmmodal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var dataid = button.data('id');
+    var modal=$(this);
+    var link=document.getElementById('delete');
+    link.setAttribute('href', '/sn/'+sn_id+'/'+loc+'/delete/'+dataid+'/')
+  });
+}
