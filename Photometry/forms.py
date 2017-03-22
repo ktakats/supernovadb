c7e306d38d1ad7fe88132e7318f611ab09725a9e
ktakats <ktakats@gmail.com>
2017-03-22 19:32:42 -0300

    added help text to photometry file upload

--- Photometry/forms.py
+++ Photometry/forms.py
@@ -39,4 +39,4 @@ class PhotometryForm(forms.models.ModelForm):
         return phot
 
 class UploadPhotometryFileForm(forms.Form):
-    file=forms.FileField()
+    file=forms.FileField(help_text="First line is the header, first column is MJD, then the filter names, and errors. Last column is optionally notes.\n Filter names have to be as in the form above.\n Format of the first line e.g.: 'MJD B B_err V V_err R R_err I I_err notes' (do not comment it out!)\n Separator has to be a space \n Notes has to be in quotes")
