24e58b1b32cf5c062f273cd96402ece2ef71828a
ktakats <ktakats@gmail.com>
2017-03-22 18:39:38 -0300

    FT passing

--- functional_tests/test_photometry.py
+++ functional_tests/test_photometry.py
@@ -32,7 +32,6 @@ class PhotometryTest(FunctionalTest):
 
         #He sees that he can add photometry by uploading a file
         self.browser.find_element_by_id("id_file").send_keys("/home/kati/Dropbox/munka/learning/sn_app/test_tools/photometry.txt")
-        time.sleep(5)
         self.browser.find_element_by_id('id_uploadbutton').click()
 
         #After sending the file, the data appears in a table
@@ -42,9 +41,7 @@ class PhotometryTest(FunctionalTest):
         #He uploads another file, however the format of this is incorrect, so he gets an error
         self.browser.find_element_by_id("id_file").send_keys("/home/kati/Dropbox/munka/learning/sn_app/test_tools/photometry_incorrect.txt")
         self.browser.find_element_by_id('id_uploadbutton').click()
-        time.sleep(5)
         body=self.browser.find_element_by_tag_name("body").text
-        time.sleep(5)
         self.assertIn("The file format is incorrect. Please check the requirements", body)
 
     def test_user_can_edit_and_delete_photometric_point(self):
@@ -78,7 +75,7 @@ class PhotometryTest(FunctionalTest):
 
         #He also realizes that he can simply delete this entry
         self.browser.find_element_by_css_selector(".fa-trash-o").click()
-        time.sleep(5)
+        time.sleep(2)
         self.browser.find_element_by_id("id_deletebutton").click()
         body=self.browser.find_element_by_tag_name("body").text
         self.assertNotIn("15.4", body)
@@ -95,4 +92,5 @@ class PhotometryTest(FunctionalTest):
 
         #He sees a button promising to plot the light curve
         self.browser.find_element_by_id("id_plotbutton").click()
-        self.fail()
+        buttontext=self.browser.find_element_by_id("id_plotbutton").text
+        self.assertEqual(buttontext, "Hide light curve")
