cd28bfec7ee8ebc9a42d0a6bfa29e31e289bed63
ktakats <ktakats@gmail.com>
2017-03-24 14:24:34 -0300

    Spectroscopy file upload form has help_text

--- Spectroscopy/forms.py
+++ Spectroscopy/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class UploadSpectrumForm(forms.Form):
     MJD=forms.DecimalField(max_digits=7, decimal_places=2)
-    file=forms.FileField()
+    file=forms.FileField(help_text="File needs two columns:\n First is wavelength, second is log10(Flux) (Do not use scientific notation!)\n Sparator is space")
     notes=forms.CharField(required=False, widget=forms.Textarea(attrs={'rows': '3', 'cols': '25'}))
--- Spectroscopy/tests/test_forms.py
+++ Spectroscopy/tests/test_forms.py
@@ -6,3 +6,7 @@ class UploadSpectrumFormTest(TestCase):
     def test_default(self):
         form=UploadSpectrumForm()
         self.assertIn('File', form.as_p())
+
+    def test_form_has_help_text(self):
+        form=UploadSpectrumForm()
+        self.assertIn("File needs two columns:\n First is wavelength, second is log10(Flux) (Do not use scientific notation!)\n Sparator is space", form.as_p())
