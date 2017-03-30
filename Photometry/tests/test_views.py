46656fb5d00bf7d8bb0ca66e8f7a18f213609ae1
ktakats <ktakats@gmail.com>
2017-03-30 18:08:11 -0300

    Photometry test refactoring

--- Photometry/tests/test_views.py
+++ Photometry/tests/test_views.py
@@ -5,35 +5,29 @@ from django.contrib import auth
 
 User=auth.get_user_model()
 
+def login_and_create_SN(self, name='SN 2017A', ra=22.625, dec=65.575):
+    user=User.objects.create_user(username='test@test.com', password="bla")
+    self.client.force_login(user)
+    sn=SN.objects.create(sn_name=name, ra=ra, dec=dec, pi=user)
+    return sn
+
 class PhotometryViewTest(TestCase):
 
-    def test_view_uses_photometry_template(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+    def test_view_uses_photometry_template_renders_form(self):
+        sn=login_and_create_SN(self)
         response=self.client.get('/sn/%d/photometry/' % (sn.id))
         self.assertTemplateUsed(response, 'photometry.html')
-
-    def test_view_renders_form(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
-        response=self.client.get('/sn/%d/photometry/' % (sn.id))
         self.assertContains(response, "id_MJD")
 
     def test_view_renders_table(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         phot=Photometry.objects.create(sn=sn, MJD=53003.5, Filter='V', magnitude=16.7, mag_error=0.02, notes="this sn")
         response=self.client.get('/sn/%d/photometry/' % (sn.id))
         self.assertContains(response, 'table-container')
         self.assertContains(response, '53003.5')
 
     def test_view_requires_login(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         phot=Photometry.objects.create(sn=sn, MJD=53003.5, Filter='V', magnitude=16.7, mag_error=0.02, notes="this sn")
         self.client.logout()
         response=self.client.get('/sn/%d/photometry/' % (sn.id))
@@ -42,26 +36,20 @@ class PhotometryViewTest(TestCase):
 class editPhotViewTest(TestCase):
 
     def test_edit_fills_out_phot_form(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         phot=Photometry.objects.create(sn=sn, MJD=53003.5, Filter='V', magnitude=16.7, mag_error=0.02, notes="this sn")
         response=self.client.get('/sn/%d/photometry/edit/%d/' % (sn.id, phot.id))
         form=response.context['form']
         self.assertIn('value="16.7"', form.as_p())
 
     def test_redirects_to_phot_page_after_editing(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         phot=Photometry.objects.create(sn=sn, MJD=53003.5, Filter='V', magnitude=16.7, mag_error=0.02, notes="this sn")
         response=self.client.post('/sn/%d/photometry/edit/%d/' % (sn.id, phot.id), data={'sn':sn, 'MJD':53003.5, 'Filter':'V', 'magnitude':15.7, 'mag_error': 0.02, 'notes': "this sn"})
         self.assertRedirects(response, '/sn/%d/photometry/' % (sn.id))
 
     def test_view_requires_login(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         phot=Photometry.objects.create(sn=sn, MJD=53003.5, Filter='V', magnitude=16.7, mag_error=0.02, notes="this sn")
         self.client.logout()
         response=self.client.post('/sn/%d/photometry/edit/%d/' % (sn.id, phot.id), data={'sn':sn, 'MJD':53003.5, 'Filter':'V', 'magnitude':15.7, 'mag_error': 0.02, 'notes': "this sn"})
@@ -71,24 +59,20 @@ class editPhotViewTest(TestCase):
 class deletePhotViewTest(TestCase):
 
     def test_can_delete_photometry_entry(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         phot=Photometry.objects.create(sn=sn, MJD=53003.5, Filter='V', magnitude=16.7, mag_error=0.02, notes="this sn")
         response=self.client.get('/sn/%d/photometry/delete/%d/' % (sn.id, phot.id))
         self.assertEqual(Photometry.objects.count(), 0)
 
     def test_after_deletion_redirects_to_photometry_page(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         phot=Photometry.objects.create(sn=sn, MJD=53003.5, Filter='V', magnitude=16.7, mag_error=0.02, notes="this sn")
         response=self.client.post('/sn/%d/photometry/delete/%d/' % (sn.id, phot.id))
         self.assertRedirects(response, '/sn/%d/photometry/' % (sn.id))
 
     def test_view_requires_login(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
+        self.client.logout()
         phot=Photometry.objects.create(sn=sn, MJD=53003.5, Filter='V', magnitude=16.7, mag_error=0.02, notes="this sn")
         response=self.client.post('/sn/%d/photometry/delete/%d/' % (sn.id, phot.id))
         self.assertRedirects(response, '/?next=/sn/%d/photometry/delete/%d/' % (sn.id, phot.id))
@@ -97,16 +81,14 @@ class deletePhotViewTest(TestCase):
 class queryViewTest(TestCase):
 
     def test_query_returns_photometry_data(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         phot=Photometry.objects.create(sn=sn, MJD=53003.5, Filter='V', magnitude=16.7, mag_error=0.02, notes="this sn")
         response=self.client.get('/sn/%d/photometry/query/' % (sn.id))
         self.assertContains(response, "53003.50")
 
     def test_view_requires_login(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
+        self.client.logout()
         phot=Photometry.objects.create(sn=sn, MJD=53003.5, Filter='V', magnitude=16.7, mag_error=0.02, notes="this sn")
         response=self.client.get('/sn/%d/photometry/query/' % (sn.id))
         self.assertRedirects(response, '/?next=/sn/%d/photometry/query/' % (sn.id))
