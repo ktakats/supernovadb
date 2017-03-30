b7ac94772508e56450295c53832c67b6c369bded
ktakats <ktakats@gmail.com>
2017-03-30 18:00:49 -0300

    Spectroscopy test refactoring

--- Spectroscopy/tests/test_views.py
+++ Spectroscopy/tests/test_views.py
@@ -5,41 +5,29 @@ from django.contrib import auth
 
 User=auth.get_user_model()
 
+def login_and_create_SN(self, name='SN 2017A', ra=22.625, dec=65.575):
+    user=User.objects.create_user(username='test@test.com', password="bla")
+    self.client.force_login(user)
+    sn=SN.objects.create(sn_name=name, ra=ra, dec=dec, pi=user)
+    return sn
+
 class SpectroscopyViewTest(TestCase):
 
-    def test_view_uses_spectroscopy_template(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+    def test_view_uses_spectroscopy_template_and_renders_form(self):
+        sn=login_and_create_SN(self)
         response=self.client.get('/sn/%d/spectroscopy/' % (sn.id))
         self.assertTemplateUsed(response, 'spectroscopy.html')
-
-    def test_view_returns_correct_sn_name(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
-        response=self.client.get('/sn/%d/spectroscopy/' % (sn.id))
         self.assertContains(response, 'SN 2017A')
-
-    def test_view_renders_form(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
-        response=self.client.get('/sn/%d/spectroscopy/' % (sn.id))
         self.assertContains(response, "id_file")
 
     def test_uploading_file_redirects_back(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         myfile=open('/home/kati/Dropbox/munka/learning/sn_app/test_tools/test_spectrum.dat')
         response=self.client.post('/sn/%d/spectroscopy/' % (sn.id), {'file': myfile, 'MJD': 55055.0, 'notes': ""})
         self.assertRedirects(response, '/sn/%d/spectroscopy/' % (sn.id))
 
     def test_view_renders_table(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         myfile=open('/home/kati/Dropbox/munka/learning/sn_app/test_tools/test_spectrum.dat')
         self.client.post('/sn/%d/spectroscopy/' % (sn.id), {'file': myfile, 'MJD': 55055.0, 'notes': ""})
         response=self.client.get('/sn/%d/spectroscopy/' % (sn.id))
@@ -47,37 +35,24 @@ class SpectroscopyViewTest(TestCase):
         self.assertContains(response, '55055.0')
 
     def test_view_requires_login(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
+        self.client.logout()
         response=self.client.get('/sn/%d/spectroscopy/' % (sn.id))
         self.assertRedirects(response, "/?next=/sn/%d/spectroscopy/" % (sn.id))
 
 class deleteSpectrumViewTest(TestCase):
 
-    def test_can_delete_spectrum(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+    def test_can_delete_spectrum_then_redirects_to_spectroscopy_page(self):
+        sn=login_and_create_SN(self)
         myfile=open('/home/kati/Dropbox/munka/learning/sn_app/test_tools/test_spectrum.dat')
         self.client.post('/sn/%d/spectroscopy/' % (sn.id), {'file': myfile, 'MJD': 55055.0, 'notes': ""})
         sp=Spectrum.objects.first()
         response=self.client.get('/sn/%d/spectroscopy/delete/%d/' % (sn.id, sp.id))
         self.assertEqual(Spectrum.objects.count(), 0)
-
-    def test_after_deletion_redirects_to_spectroscopy_page(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
-        myfile=open('/home/kati/Dropbox/munka/learning/sn_app/test_tools/test_spectrum.dat')
-        self.client.post('/sn/%d/spectroscopy/' % (sn.id), {'file': myfile, 'MJD': 55055.0, 'notes': ""})
-        sp=Spectrum.objects.first()
-        response=self.client.get('/sn/%d/spectroscopy/delete/%d/' % (sn.id, sp.id))
         self.assertRedirects(response, '/sn/%d/spectroscopy/' % (sn.id))
 
     def test_view_requires_login(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         myfile=open('/home/kati/Dropbox/munka/learning/sn_app/test_tools/test_spectrum.dat')
         self.client.post('/sn/%d/spectroscopy/' % (sn.id), {'file': myfile, 'MJD': 55055.0, 'notes': ""})
         sp=Spectrum.objects.first()
@@ -88,9 +63,7 @@ class deleteSpectrumViewTest(TestCase):
 class queryViewTest(TestCase):
 
     def test_query_returns_spectroscopy_data(self):
-        user=User.objects.create_user(username='test@test.com', password="bla")
-        self.client.force_login(user)
-        sn=SN.objects.create(sn_name='SN 2017A', ra=22.625, dec=65.575, pi=user)
+        sn=login_and_create_SN(self)
         myfile=open('/home/kati/Dropbox/munka/learning/sn_app/test_tools/test_spectrum.dat')
         self.client.post('/sn/%d/spectroscopy/' % (sn.id), {'file': myfile, 'MJD': 55055.0, 'notes': ""})
         myfile=open('/home/kati/Dropbox/munka/learning/sn_app/test_tools/test_spectrum2.dat')
@@ -98,3 +71,13 @@ class queryViewTest(TestCase):
         response=self.client.get('/sn/%d/spectroscopy/query/' % (sn.id))
         self.assertContains(response, "55055.0")
         self.assertContains(response, "55045.0")
+
+    def test_view_requires_login(self):
+        sn=login_and_create_SN(self)
+        myfile=open('/home/kati/Dropbox/munka/learning/sn_app/test_tools/test_spectrum.dat')
+        self.client.post('/sn/%d/spectroscopy/' % (sn.id), {'file': myfile, 'MJD': 55055.0, 'notes': ""})
+        myfile=open('/home/kati/Dropbox/munka/learning/sn_app/test_tools/test_spectrum2.dat')
+        self.client.post('/sn/%d/spectroscopy/' % (sn.id), {'file': myfile, 'MJD': 55045.0, 'notes': ""})
+        self.client.logout()
+        response=self.client.get('/sn/%d/spectroscopy/query/' % (sn.id))
+        self.assertRedirects(response, '/?next=/sn/%d/spectroscopy/query/' % (sn.id))
