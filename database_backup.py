5b9fda3835f99498565197a65971615ff379e8ce
ktakats <ktakats@gmail.com>
2017-05-15 19:54:56 -0400

    backup script

---
+++ database_backup.py
@@ -0,0 +1,12 @@
+from decouple import config
+import os
+from time import strftime
+
+BACKUPPATH="./backup/dbbackup/"+ strftime("%y%m%d-%H%M%S") + ".sql.gz"
+
+dumpscrip="mysqldump -u" + config('DB_USER') + " -p" + config('DB_PWD') + " -h " + config('DB_HOST') + " --databases " + config('DB_NAME') + " | gzip -9 >"  + BACKUPPATH
+
+os.system(dumpscrip)
+
+#http://webcheatsheet.com/sql/mysql_backup_restore.php
+#mysql -u [uname] -p[pass] [db_to_restore] < [backupfile.sql]
